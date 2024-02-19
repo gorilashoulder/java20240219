b50f217026325de4c8cdfe9ebc2c21f2c19c20d3
sunghwan <simcom0820@naver.com>
2024-02-19 16:01:45 +0900

    변수 타입

---
+++ src/c02/lecture/p02type/c02Integer.java
@@ -0,0 +1,14 @@
+package c02.lecture.p02type;
+
+public class c02Integer {
+    public static void main(String[] args) {
+        //정수타입
+        //int
+        int a =5; int b= 30;
+        b=3000; b=30000;
+
+        //long
+
+        //byte, short
+    }
+}
