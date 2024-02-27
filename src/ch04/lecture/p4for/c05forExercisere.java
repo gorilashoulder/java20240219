ba593e7a10c35618a13eb8cebca8c0f23fc85a23
sunghwan <simcom0820@naver.com>
2024-02-26 21:18:08 -0800

    hell

--- src/ch04/lecture/p4for/c05forExercisere.java
+++ src/ch04/lecture/p4for/c05forExercisere.java
@@ -79,5 +79,16 @@ public class c05forExercisere {
             System.out.println();
         }
 
+        System.out.println("------------------");
+        for(int i=0; i<5; i++){
+            for (int j=0; j<5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
     }
 }
