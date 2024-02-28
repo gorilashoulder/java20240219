1522229e3b41df92db7263b47f49a3958fb92258
sunghwan <simcom0820@naver.com>
2024-02-27 20:45:29 -0800

    hell

--- src/ch05/lecture/p1array/c08Array.java
+++ src/ch05/lecture/p1array/c08Array.java
@@ -1,6 +1,6 @@
 package ch05.lecture.p1array;
 
-public class c08Arrayre {
+public class c08Array {
     public static void main(String[] args) {
         int arr[]= new int[]{1,2,3,4,5,642};
 
@@ -19,7 +19,20 @@ public class c08Arrayre {
             System.out.println(item);
         }
 
-        //각 원소를 두배한 값으로 교체
+        // 각 원소를 두배한 값으로 교체
+        // 코드 작성
+//        arr[0] = arr[0] * 2;
+//        arr[1] = arr[1] * 2;
+//        arr[2] = arr[2] * 2;
+//        arr[arr.length - 1] = arr[arr.length - 1] * 2;
+        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = arr[i] * 2;
+            arr[i] *= 2;
+        }
 
+        System.out.println("-----------------");
+        for (int item : arr) {
+            System.out.println(item);
+        }
     }
 }
--- src/ch05/lecture/p1array/c11Arrayre.java
+++
@@ -1,7 +0,0 @@
-package ch05.lecture.p1array;
-
-public class c11Arrayre {
-    public static void main(String[] args) {
-
-    }
-}
