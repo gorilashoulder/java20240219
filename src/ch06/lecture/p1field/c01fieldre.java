828c4fe7a589dbfcc01dbb1d73853dd79cd01943
sunghwan <simcom0820@naver.com>
2024-03-04 21:13:27 -0800

    hell

--- src/ch06/lecture/p1field/c01fieldre.java
+++ src/ch06/lecture/p1field/c01fieldre.java
@@ -6,5 +6,12 @@ public class c01fieldre {
         int[]arr1=new int[3];
         int[]arr2=new int[5];
 
+
+        System.out.println(arr1.length);
+        System.out.println(arr2.length);
+
+        System.out.println(arr1.hashCode());
+        System.out.println(arr2.hashCode());
+
     }
 }
