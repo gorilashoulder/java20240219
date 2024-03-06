a5533b339ebefc64747b281b312b6d6caf5e575c
sunghwan <simcom0820@naver.com>
2024-03-06 16:45:40 +0900

    두번째

--- src/ch06/lecture/p7access/exam20/Account.java
+++ src/ch06/lecture/p7access/exam20/Account.java
@@ -1,16 +1,8 @@
 package ch06.lecture.p7access.exam20;
 
-import java.util.Scanner;
+
 
 public class Account {
-    public static void main(String[] args) {
-        System.out.println("____________");
-        System.out.println("1.계좌생성 ㅣ 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
-        System.out.println("____________");
-        Scanner scanner=new Scanner(System.in);
 
-        switch (){
-
-        }
-    }
 }
+
--- src/ch06/lecture/p7access/exam20/BankApplication.java
+++ src/ch06/lecture/p7access/exam20/BankApplication.java
@@ -2,8 +2,4 @@ package ch06.lecture.p7access.exam20;
 
 public class BankApplication {
 
-
 }
-
-
-
