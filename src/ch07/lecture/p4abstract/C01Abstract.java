bc4d6a6418c9c2f3cedd14ab663ec03e9e0f08cd
sunghwan <simcom0820@naver.com>
2024-03-08 21:42:47 -0800

    hell

--- src/ch07/lecture/p4abstract/C01Abstract.java
+++ src/ch07/lecture/p4abstract/C01Abstract.java
@@ -1,4 +1,12 @@
 package ch07.lecture.p4abstract;
 
 public class C01Abstract {
+    public static void main(String[] args) {
+        C01Animal sohee=new C01Tiger();
+        C01Animal tiger=new C01Tiger();
+    }
+    //추상 클래스로 인스턴스 생성불가!
+    //C01Animal animal1=new C01Animal();
 }
+abstract class C01Animal{}
+class C01Tiger extends C01Animal{}
--- src/ch07/lecture/p4abstract/C02Abstract.java
+++ src/ch07/lecture/p4abstract/C02Abstract.java
@@ -1,4 +1,39 @@
 package ch07.lecture.p4abstract;
 
 public class C02Abstract {
+    public static void main(String[] args) {
+        C02Animal fish=new C02Fish();
+        fish.breathe();
+        C02Animal cat=new C02Cat();
+        cat.breathe();
+
+        C02Animal b3=new C02Bird();
+    }
 }
+abstract class C02Animal{
+    //abstract method:몸통 없는 메소드
+    //하위 클래스가 꼭 재정의 되야함
+    //추상메소드가 있다면 클래스는 꼭 추상 클래스
+     abstract public void breathe();
+
+}
+class C02Fish extends C02Animal{
+    @Override
+    public void breathe() {
+        System.out.println("아가미호흡");
+    }
+}
+class C02Cat extends C02Animal{
+    @Override
+    public void breathe() {
+        System.out.println("폐호흡");
+
+    }
+}
+class C02Bird extends C02Animal{
+    @Override
+    public void breathe() {
+
+    }
+}
+
