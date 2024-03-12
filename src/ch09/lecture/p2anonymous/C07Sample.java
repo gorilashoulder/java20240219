888a7d5beef94e2fcd35e83ce1725e5eefac274f
sunghwan <simcom0820@naver.com>
2024-03-12 16:26:01 +0900

    추상메소드

--- src/ch09/lecture/p2anonymous/C07Sample.java
+++ src/ch09/lecture/p2anonymous/C07Sample.java
@@ -1,15 +1,30 @@
 package ch09.lecture.p2anonymous;
 
+
 import java.util.Random;
 
 public class C07Sample {
     public static void main(String[] args) {
         //TODO:null 대신 익명클래스 객체 코드 작성
 
-        Game rpsGame=null;
+        Game rpsGame=new Game() {
+            @Override
+            public void play() {
+                String [] rps={"주먹","가위","바위"};
+                int random= new Random().nextInt(rps.length);
+                System.out.println(rps[random]);
+            }
+        };
         rpsGame.play();//가위 바위 보 중 하나 출력
 
-        Game lottoGame=null;
+        Game lottoGame=new Game() {
+            @Override
+            public void play() {
+//                int[] lotto=new int[45];
+                int random=new Random().nextInt(45)+1;
+                System.out.println(random);
+            }
+        };
         //초보자 :1~45 값 중 한개 출ㄹ력
         lottoGame.play();
     }
