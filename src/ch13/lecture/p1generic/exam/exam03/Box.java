995fa065fc0cad32235523e9a15e93b320dbac68
sunghwan <simcom0820@naver.com>
2024-03-15 16:53:37 +0900

    제네릭~

---
+++ src/ch13/lecture/p1generic/exam/exam03/Box.java
@@ -0,0 +1,31 @@
+package ch13.lecture.p1generic.exam.exam03;
+
+public class Box <T>{
+    public T content;
+
+    public boolean compare(Box<T>other){
+        boolean result= content.equals(other.content);
+        return result;
+    }
+}
+
+class GenericExample{
+    public static void main(String[] args) {
+        //타입 파라미터에 타입을 명시 하지 않으면 Object로 결정됨
+        Box box1 =new Box();
+        box1.content="100";
+
+        Box box2=new Box();
+        box2.content="100";
+
+        Box box3=new Box();
+        box3.content=100;
+
+        boolean result1=box1.compare(box2);
+        System.out.println(result1);
+
+        boolean result2=box1.compare(box3);
+        System.out.println(result2);
+
+    }
+}
