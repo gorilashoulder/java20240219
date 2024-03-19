ed2ae1d9fe1735aec7d49e4b44d8742308646fc1
sunghwan <simcom0820@naver.com>
2024-03-19 17:47:24 +0900

    제네릭~

---
+++ src/ch15/lecture/p2set/excercise/n8/Student.java
@@ -0,0 +1,33 @@
+package ch15.lecture.p2set.excercise.n8;
+
+import java.util.Objects;
+
+public class Student {
+    public int studentNum;
+    public String name;
+
+    public Student(int studentNum, String name) {
+        this.studentNum = studentNum;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentNum=" + studentNum +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentNum == student.studentNum ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentNum);
+    }
+}
