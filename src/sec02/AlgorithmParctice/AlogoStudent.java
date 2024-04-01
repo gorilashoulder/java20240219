cd17e42a991f3fadd26005049cb11a74f5b3dc4f
sunghwan <simcom0820@naver.com>
2024-04-01 22:55:43 +0900

    제네릭~

---
+++ src/sec02/AlgorithmParctice/AlogoStudent.java
@@ -0,0 +1,27 @@
+package sec02.AlgorithmParctice;
+
+public class AlogoStudent {
+    String name ;
+    String no;
+
+    public AlogoStudent(String name, String no){
+        this.name=name;
+        this.no=no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String student) {
+        this.name = student;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+}
---
+++ src/sec02/AlgorithmParctice/algorithm.java
@@ -0,0 +1,47 @@
+package sec02.AlgorithmParctice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class algorithm {
+    public static void main(String[] args) {
+        AlogoStudent alogoStudent = new AlogoStudent("심성환", "2015");
+        AlogoStudent alogoStudent1 = new AlogoStudent("서다봄", "2016");
+        AlogoStudent alogoStudent2 = new AlogoStudent("박재훈", "2017");
+
+        ArrayList<AlogoStudent> list = new ArrayList<>();
+        list.add(alogoStudent);
+        list.add(alogoStudent1);
+        list.add(alogoStudent2);
+
+
+
+
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("y는 반복 N은종료");
+        String line = scanner.nextLine();
+        while (true){
+            if (line.equals("y")){
+                System.out.println("검색시작");
+                String name= scanner.next();
+                boolean flag=false;
+                for (AlogoStudent member : list){
+                   if (member.getName().equals(name)){
+                       System.out.println("감기걸렸다");
+                       flag=true;
+                   }
+                }
+                if (!flag){
+                    System.out.println("끝");
+
+            }
+            }
+
+        }
+
+    }
+    }
